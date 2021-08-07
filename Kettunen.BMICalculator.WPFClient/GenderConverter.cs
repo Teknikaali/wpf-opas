cb3483eb60a1da9749af96436a9da630f07e6e60
Anssi Kettunen <Teknikaali@users.noreply.github.com>
2021-08-07 17:17:26 +0300

    Fix GenderConverter to use Gender instead of string.

--- Kettunen.BMICalculator.WPFClient/GenderConverter.cs
+++ Kettunen.BMICalculator.WPFClient/GenderConverter.cs
@@ -8,11 +8,11 @@ namespace Kettunen.BMICalculator.WPFClient
     public class GenderConverter : MarkupExtension, IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            => (Gender)value == (Gender)Enum.Parse(typeof(Gender), parameter as string);
+            => (Gender)value == (Gender)parameter;
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
             => (bool?)value == true
-                ? Enum.Parse(typeof(Gender), parameter as string)
+                ? parameter
                 : null;
 
         public override object ProvideValue(IServiceProvider serviceProvider)
